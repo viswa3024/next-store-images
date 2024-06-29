4d11985a12389577b7840159aa7ed292475a70ce
Recorichardretardo <hi@hi.com>
2024-06-29 18:45:04 +0530

    images nextjs configuration changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
-        loader: 'default', // or 'custom' if you want to define a custom loader function
+        // domains: ['localhost'], //The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
+        // loader: 'default', // or 'custom' if you want to define a custom loader function 
+
+        remotePatterns: [
+          {
+            protocol: 'http',
+            hostname: 'localhost',
+            pathname: '**',
+          },
+        ],
       },
 };
 
