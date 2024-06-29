b370420a8cbd0547c318ecdfdc1d7657eb023db6
Recorichardretardo <hi@hi.com>
2024-06-29 18:56:31 +0530

    env variables update

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,12 @@ const nextConfig = {
         // domains: ['localhost'], //The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
         // loader: 'default', // or 'custom' if you want to define a custom loader function 
 
+        // https://stackoverflow.com/questions/77447587/the-images-domains-configuration-is-deprecated-please-use-images-remotepatte
         remotePatterns: [
           {
-            protocol: 'http',
-            hostname: 'localhost',
-            pathname: '**',
+            protocol: process.env.NEXT_PUBLIC_API_PROTOCOL || 'http',
+            hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || 'localhost',
+            pathname: '**', //pathname: '**', // Match all paths under this configuration
           },
         ],
       },
