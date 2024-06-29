6017f1e93e475bee647391290265191c81bcdb3f
Recorichardretardo <hi@hi.com>
2024-06-29 18:39:31 +0530

    environment variables changes

--- src/app/showimages/page.tsx
+++ src/app/showimages/page.tsx
@@ -6,27 +6,37 @@ import Image from 'next/image';
 
 export default function HomePage() {
   const [images, setImages] = useState([]);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'; // Default to localhost if not set
 
   useEffect(() => {
+
+    console.log("apiUrl: ", apiUrl)
     async function fetchImages() {
-      const response = await fetch('http://localhost:8000/images');
-      const data = await response.json();
-      setImages(data.images);
+      try {
+        const response = await fetch(`${apiUrl}/images`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch images');
+        }
+        const data = await response.json();
+        setImages(data.images);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
     }
     fetchImages();
-  }, []);
+  }, [apiUrl]);
 
   return (
     <div>
       {images.map((image, index) => (
-        <div key={index}>
-          <Image
-            src={`http://localhost:8000${image}`}
+        <Image
+            key={index}
+            src={`${apiUrl}${image}`}
             alt={`Image ${index}`}
             width={500}
             height={500}
-          />
-        </div>
+            priority={index === 0} // Example: Set priority for the first image
+      />
       ))}
     </div>
   );
